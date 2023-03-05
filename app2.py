2f880924f214abef9a3c79e47a56da21e8a1749d
etotoja <albertdrozdowski@gmail.com>
2023-03-06 00:16:19 +0100

    podstawy2

---
+++ app2.py
@@ -0,0 +1,13 @@
+def greet_user(first_name, last_name):
+
+    print(f'Hi {first_name} {last_name}!')
+
+    print('welcome aboard')
+
+
+
+
+
+print('Start')
+greet_user("Peter", "Smith")
+print("Finish")
