3a59516fff6ace9ad3e13004687eae0f74f58760
etotoja <119266054+etotoja@users.noreply.github.com>
2023-03-21 20:59:45 +0100

    xxx test xxx

--- Python in progress/app.py
+++
@@ -1,8 +0,0 @@
-#proby testow automatycznych - otworzenie przegladarki Edge
-
-from selenium import webdriver
-
-driver = webdriver.Edge() #bez sciezki bo sterowniki są dodane do PATH
-driver.get("https://www.google.pl") #otworzenie danego adresu url
-while(True):
-    pass
